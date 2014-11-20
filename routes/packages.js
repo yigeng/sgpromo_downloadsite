d0aa4a7720b2a0de238739f4922478382af389f4
root <root@cv00300005125-1.z1.cloudworld.tw>
2014-11-20 17:42:34 +0800

    index.jade修改，下载包替换成vpn上包

--- routes/packages.js
+++ routes/packages.js
@@ -3,19 +3,28 @@ var router = express.Router();
 var crypto = require('crypto');
 var fs = require ('fs');
 
+apk_links = {
+    "56":"http://fastdcc.gamefirst.com.tw/soulgame/test.apk"
+}
+
 /* GET users listing. */
 router.get('/', function(req, res) {
     var channel = req.query['c'];
-    var filename = 'DuelOfHeros.apk';
-    var path = __dirname + '/../packages/'+ channel+"/"+ filename;
-    var stats = fs.statSync(path);
-    var fileSize = stats["size"];
-    res.setHeader('Content-disposition', 'attachment; filename=' + filename);
-    res.setHeader('Content-Length',fileSize);   
+//    var filename = 'DuelOfHeros.apk';
+//    var path = __dirname + '/../packages/'+ channel+"/"+ filename;
+//    var stats = fs.statSync(path);
+//    var fileSize = stats["size"];
+//    res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+//    res.setHeader('Content-Length',fileSize);   
 
-    var filestream = fs.createReadStream(path);
-    filestream.pipe(res);
 
+//    var filestream = fs.createReadStream(path);
+//    filestream.pipe(res);
+     res.writeHead(302, {
+        'Location': apk_links[channel]
+        //add other headers here...
+    });
+    res.end();
     // record the apt_downloaded event
     var now = new Date().toISOString().slice(0, 19).replace('T', ' ');
     var ip = req.headers['x-forwarded-for'] ||
